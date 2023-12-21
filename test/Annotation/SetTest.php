c968b39852bb1d20d377ed575ccf53c2c58f9fe0
bassirou ngom <bassiroungom@esp.sn>
2023-12-21 15:32:20 +0000

    update test

--- test/Annotation/SetTest.php
+++ test/Annotation/SetTest.php
@@ -13,4 +13,28 @@ class SetTest
     {
         return $this->test;
     }
+
+    public function setTest(string $test): void
+    {
+        $this->test = $test;
+    }
+
+    public function test(): void
+    {
+        $this->setTest('test');
+    }
 }
+
+// Path: test/Annotation/ToStringTest.php
+
+// Compare this snippet from test/Annotation/ToStringTest.php:
+// <?php
+// namespace Bngesp\Lombok\Test\Annotation;
+// 
+// use Bngesp\Lombok\Annotation\ToString;
+//
+// #[ToString(['test'])]
+// class ToStringTest
+// {
+//     private string $test = 'test';
+// }
