7d41cdb6d67d5efbb2c81c1694eaa6d7dbcaae0c
bassirou ngom <bassiroungom@esp.sn>
2023-12-21 15:33:00 +0000

    update test

--- test/Annotation/GetTest.php
+++ test/Annotation/GetTest.php
@@ -13,3 +13,18 @@ class GetTest
         return $this->test;
     }
 }
+
+// Path: test/Annotation/ToStringTest.php
+
+// Compare this snippet from test/Annotation/ToStringTest.php:
+
+// <?php
+// namespace Bngesp\Lombok\Test\Annotation;
+//
+// use Bngesp\Lombok\Annotation\ToString;
+//
+// #[ToString(['test'])]
+// class ToStringTest
+// {
+//     private string $test = 'test';
+// }
