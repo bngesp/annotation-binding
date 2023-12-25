a9a8887921f72ceaa7a3b940fff9423082e279a2
bassirou ngom <bassiroungom@esp.sn>
2023-12-25 19:11:07 +0100

    build tests cases

--- src/Annotation/Builder.php
+++ src/Annotation/Builder.php
@@ -1,8 +1,10 @@
 <?php
 namespace Bngesp\Lombok\Annotation;
 
+use Attribute;
 use Bngesp\Lombok\Annotation\Base\AbstractAnnotation;
-
-class Builder extends AbstractAnnotation{
+#[Attribute(Attribute::TARGET_CLASS)]
+class Builder extends AbstractAnnotation
+{
 
 }
--- src/Annotation/Method.php
+++ src/Annotation/Method.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Bngesp\Lombok\Annotations;
+namespace Bngesp\Lombok\Annotation;
 
 enum Method
 {
